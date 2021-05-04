71b65910c5962b058ce902c730fc5269e8259e2c
Carl S <a9997766@hotmail.com>
2021-05-04 23:47:16 +1000

    ta lc quote watch-widget pages mostly done

--- public/watch/watch-widget.js
+++ public/watch/watch-widget.js
@@ -1,14 +1,40 @@
-var nav = // the content - example of bootstrap's nav
-'<nav class="navbar navbar-expand-sm navbar-dark bg-dark py-1">'+
-'  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">'+
-'    <div class="navbar-nav">'+
-'      <a class="nav-item nav-link py-0" href="./btc.html">BTC</a>' +
-'      <a class="nav-item nav-link py-0" href="./eth.html">ETH</a>' +
-'      <a class="nav-item nav-link py-0" href="./xrp.html">XRP</a>' +
-'      <a class="nav-item nav-link py-0" href="./hum.html">HUM</a>' +
-'    </div>' +
-'  </div>' +
+// var nav = // the content - example of bootstrap's nav
+// '<nav class="navbar navbar-expand-sm navbar-dark bg-dark py-1">'+
+// '  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">'+
+// '    <div class="navbar-nav">'+
+// '      <a class="nav-item nav-link py-0" href="./btc.html">BTC</a>' +
+// '      <a class="nav-item nav-link py-0" href="./eth.html">ETH</a>' +
+// '      <a class="nav-item nav-link py-0" href="./xrp.html">XRP</a>' +
+// '      <a class="nav-item nav-link py-0" href="./hum.html">HUM</a>' +
+// '    </div>' +
+// '  </div>' +
+// '</nav>';
+
+const url_splits = window.location.href.split("/");
+const page_name = url_splits[url_splits.length-1];
+let chart_style_str = page_name.substring(0,2);  // either lc or ta
+
+if(chart_style_str === 'qu'){  //when going from quotes page, default go to ta
+    chart_style_str = 'ta';
+}
+
+let alt_style_page_name = '';
+if(chart_style_str === 'lc'){
+    alt_style_page_name = page_name.replace('lc', 'ta');
+}else if(chart_style_str === 'ta'){
+    alt_style_page_name = page_name.replace('ta', 'lc');
+}
+
+var nav = // non bootstrap navbar
+'<nav class="navbar">'+
+'    <a class="nav-item" href="./' + chart_style_str + 'btc.html">BTC</a>' +
+'    <a class="nav-item" href="./' + chart_style_str + 'eth.html">ETH</a>' +
+'    <a class="nav-item" href="./' + chart_style_str + 'xrp.html">XRP</a>' +
+'    <a class="nav-item" href="./' + chart_style_str + 'hum.html">HUM</a>' +
+'    <a class="nav-item" href="./quotes.html">-Quote-</a>' +
+'    <a class="nav-item" href="./' + alt_style_page_name + '">-Alt-</a>' +
 '</nav>';
+
 
 var demo = document.getElementById('navbar-container'); // target element.
 demo.insertAdjacentHTML('beforeend', nav); // add content.
