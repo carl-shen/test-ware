4127425143dd6b05a0df14dd1b5b2b18136e5e65
Carl S <a9997766@hotmail.com>
2021-06-08 23:23:04 +1000

    Removed unused testing backend script.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ import store from './store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
-// setup fake backend for testing
-// import { configureFakeBackend } from './helpers';
-// configureFakeBackend();
-
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -22,7 +16,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
