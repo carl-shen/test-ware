13310ae04c022f0aa0a92ada3175dfda37a8290e
Carl <carl.shen@hotmail.com>
2023-04-15 13:31:04 +1000

    Refresh landing page jumbotron

--- public/static_src/footer.js
+++ public/static_src/footer.js
@@ -1,14 +1,12 @@
+var footer =
+  '<div class="text-center p-2 m-0">' +
+  '  <p class="text-muted m-2">&copy; 2023 Test-Ware.com</p>' +
+  '  <hr class="m-0">' +
+  "</div>" +
+  '<div class="text-center p-1 pb-5">' +
+  '  <small class="text-muted">This website is provided for entertainment use only. No information constitutes financial advice. Performance scores in this simulation may not reflect potential performance in real markets.</small>' +
+  "  <br>" +
+  '  <small class="text-muted">Financial market conditions change constantly and risk is high. Please consult professional financial advisors before trading in the real markets.</small>' +
+  "</div>";
 
-var footer =  
-'<div class="text-center p-2 m-0">' + 
-'  <p class="text-muted m-2">&copy; 2021 Test-Ware.com</p>' +
-'  <hr class="m-0">' +
-'</div>' +
-'<div class="text-center p-1 pb-5">' +
-'  <small class="text-muted">This website is provided for entertainment use only. No information constitutes financial advice. Performance scores in this simulation may not reflect potential performance in real markets.</small>' +
-'  <br>' +
-'  <small class="text-muted">Financial market conditions change constantly and risk is high. Please consult professional financial advisors before trading in the real markets.</small>' +
-'</div>';
-
-
-document.querySelector('#footer-id').insertAdjacentHTML('beforeend', footer);
+document.querySelector("#footer-id").insertAdjacentHTML("beforeend", footer);
