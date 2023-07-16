b009feac96e080b5e3fb989d4dc819f32e47abf7
Carl <carl.shen@hotmail.com>
2023-07-16 20:58:54 +1000

    Upgrade Stepper responsive design to use flexbox and fontawesome

--- src/_components/Stepper.js
+++ src/_components/Stepper.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { statsActions } from "../_actions";
 import { usePortraitMode } from "./";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 // By default, the steps to move forward is 3, i.e. 3 candles.
 const DEFAULT_STEP_SIZE = 3;
@@ -27,25 +29,31 @@ function Stepper() {
   };
 
   const stepButtonClassNames =
-    "relative inline-block w-24 px-auto py-2 mr-1 mb-1 overflow-hidden text-base font-normal text-gray-200 uppercase rounded-lg bg-gradient-to-br from-green-500 to-blue-500 hover:from-blue-500 hover:to-green-400 hover:text-white";
+    "flex-1 min-w-stepper-button relative inline-block w-24 px-auto py-2 mr-1 mb-1 overflow-hidden text-base font-normal text-gray-200 uppercase rounded-lg bg-gradient-to-br from-green-500 to-blue-500 hover:from-blue-500 hover:to-green-400 hover:text-white";
 
   return (
-    <div className="flex flex-wrap">
+    <div className="flex flex-wrap min-w-xxs">
       <button className={stepButtonClassNames} onClick={() => nextNBar(1)}>
-        <span>Next bar</span>
+        <span>
+          <FontAwesomeIcon icon={icon({ name: "angle-right" })} />
+        </span>
       </button>
       <button className={stepButtonClassNames} onClick={() => nextNBar(2)}>
-        <span>Next 2</span>
+        <span>
+          <FontAwesomeIcon icon={icon({ name: "angles-right" })} />
+        </span>
       </button>
       <input
-        className="relative inline-block w-24 pl-3 text-center py-2 mr-1 mb-1 overflow-hidden text-base font-normal text-gray-200 rounded-lg bg-gradient-to-br from-green-500 to-blue-500"
+        className="flex-1 min-w-stepper-button relative inline-block w-24 pl-3 text-center py-2 mr-1 mb-1 overflow-hidden text-base font-normal text-gray-200 rounded-lg bg-gradient-to-br from-green-500 to-blue-500"
         type="number"
         name="nsteps"
         value={nsteps}
         onChange={handleNStepsChange}
       />
       <button className={stepButtonClassNames} onClick={() => nextNBar(nsteps)}>
-        <span>Next N</span>
+        <span>
+          <FontAwesomeIcon icon={icon({ name: "angles-right" })} /> N
+        </span>
       </button>
     </div>
   );
